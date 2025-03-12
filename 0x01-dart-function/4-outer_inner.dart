cc4c535fb06d32a3cc5164a2faf7c9f78740566f
GoldenHatchet15 <raphsant20@gmail.com>
2025-03-12 14:46:45 -0400

    Outer Inner

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,6 +1,11 @@
-void outer(String name, String id){
-    String inner(){
-        return "Hello $name, your id is $id";
+void outer(String name, String id) {
+    String inner() {
+        List<String> parts = name.split(" ");// split the name by space
+        String firstName = parts[0];// get the first part
+        String lastName = parts[1];// get the second part
+
+        return "Hello Agent ${lastName[0]}.$firstName your id is $id";
     }
-    print(inner());
+
+    print(inner()); 
 }
