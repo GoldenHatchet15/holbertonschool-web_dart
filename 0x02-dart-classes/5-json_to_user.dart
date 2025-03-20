915748af31b72d0cc034e5116e0651437370ab9a
GoldenHatchet15 <raphsant20@gmail.com>
2025-03-20 16:12:14 -0400

    Json to User fix3

--- 0x02-dart-classes/5-json_to_user.dart
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -1,36 +1,35 @@
 class User {
-  int id=0;
+  int id = 0;
   String name = "";
-  int age = 0;
-  double height = 0.0;
+  int age;
+  double height;
 
-
-  // Constructor with named parameters
+  // Constructor
   User({required this.id, required this.name, required this.age, required this.height});
 
-  // Convert object to JSON (Map representation)
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'age': age,
-      'height': height
-    };
-  }
-
-  // Create User object from JSON (Map)
+  // Convert JSON (Map) to User object
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
-      id: userJson['id'], 
+      id: userJson['id'],
       name: userJson['name'],
       age: userJson['age'],
       height: userJson['height'],
     );
   }
 
-  // Override toString() for formatted output
+  // Convert User object to JSON (Map)
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'age': age,
+      'height': height,
+    };
+  }
+
+  // Override toString method
   @override
   String toString() {
-    return "User(id : $id ,name: $name,  age: $age, height: $height)";
+    return 'User(id : $id ,name: $name, age: $age, height: $height)';
   }
 }
